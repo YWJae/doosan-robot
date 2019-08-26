158542a992865a7542eca39e9d490dbdb721cfae
YWJae <wonjae.yun@gmail.com>
2019-08-26 17:07:22 +0900

    joystick 수정중(미완)

--- dsr_example/py/scripts/demo/joy_xbox360.py
+++ dsr_example/py/scripts/demo/joy_xbox360.py
@@ -25,6 +25,31 @@ DR_init.__dsr__id = ROBOT_ID
 DR_init.__dsr__model = ROBOT_MODEL
 from DSR_ROBOT import *
 
+ROBOT_SPEED_LINEAR  = 60 [mm/s]
+ROBOT_SPEED_ANGULAR = 10 [deg/s]
+
+BOTTON_A           = 0
+BOTTON_B           = 1
+BOTTON_X           = 2
+BOTTON_Y           = 3
+BOTTON_UPPER_LEFT  = 4
+BOTTON_UPPER_RIGHT = 5
+BOTTON_BACK        = 6
+BOTTON_START       = 7
+BOTTON_CENTER      = 8
+BOTTON_JOY_LEFT    = 9
+BOTTON_JOY_RIGHT   = 10
+
+AXIS_LEFT_H        = 0
+AXIS_LEFT_V        = 1
+AXIS_UPPER_LEFT    = 2
+AXIS_RIGHT_H       = 3
+AXIS_RIGHT_V       = 4
+AXIS_UPPER_RIGHT   = 5
+AXIS_DIR_H         = 6
+AXIS_DIR_V         = 7
+
+
 def shutdown():
     print "shutdown time!"
     print "shutdown time!"
@@ -58,7 +83,7 @@ msgRobotState_cb.count = 0
 def thread_subscriber():
     rospy.Subscriber('/'+ROBOT_ID +ROBOT_MODEL+'/state', RobotState, msgRobotState_cb)
     rospy.spin()
-    #rospy.spinner(2)    
+    #rospy.spinner(2)    -
 
 r = CDsrRobot(ROBOT_ID, ROBOT_MODEL)
 
@@ -72,64 +97,64 @@ def joy_cb(msg):
     targetPos = [0, 0, 90, 0, 90, 0]
     hommingPos = [0, 0, 0, 0, 0, 0]
 ##
-    if msg.buttons[7] == 1 and msg.buttons[6] == 1:
+    if msg.axes[BOTTON_START] == 1 and msg.axes[BOTTON_BACK] == 1:
         r.movej(targetPos, 50, 50)
     elif msg.buttons[8] == 1:
         r.movej(hommingPos, 50, 50)
     
-    if msg.axes[4] != 0 or msg.axes[0] != 0 or msg.axes[1] != 0:
+    if msg.axes[AXIS_LEFT_V] != 0 or msg.axes[AXIS_LEFT_H] != 0 or msg.axes[AXIS_RIGHT_V] != 0 or msg.axes[AXIS_RIGHT_H] != 0 or msg.axes[AXIS_UPPER_LEFT] != 0 or msg.axes[AXIS_UPPER_RIGHT] != 0:
         m_joyAnalogFlag = True
     else:
         m_joyAnalogFlag = False
 
-    if msg.axes[1] != 0 or msg.axes[0] or 0:
-        if abs(msg.axes[1]) > abs(msg.axes[0]):
-            m_xyCompareFlag = False
+    if msg.axes[AXIS_LEFT_V] != 0 or msg.axes[AXIS_LEFT_H] or 0:
+        if abs(msg.axes[AXIS_LEFT_V]) > abs(msg.axes[AXIS_LEFT_H]):
+            m_xyComparekkFlag = False
         else:
             m_xyCompareFlag = True
     
-    if msg.axes[6] != 0 or msg.axes[7] != 0:
+    if msg.axes[AXIS_DIR_H] != 0 or msg.axes[AXIS_DIR_V] != 0:
         m_joyButtonFlag = True
     else:
         m_joyButtonFlag = False
 
     if m_joyJogFlag == -1 and not m_joyAnalogFlag and m_joyButtonFlag:
-        if msg.axes[6] == 1:
+        if msg.axes[AXIS_DIR_H] == 1:
             m_joyJogFlag = JOG_AXIS_TASK_Y
-            m_joyJogVel  = -60
-        if msg.axes[6] == -1:
+            m_joyJogVel  = -ROBOT_SPEED_LINEAR
+        if msg.axes[AXIS_DIR_H] == -1:
             m_joyJogFlag = JOG_AXIS_TASK_Y
-            m_joyJogVel  = 60
+            m_joyJogVel  = ROBOT_SPEED_LINEAR
             
-        if msg.axes[7] == 1:
+        if msg.axes[AXIS_DIR_V] == 1:
             m_joyJogFlag = JOG_AXIS_TASK_X
-            m_joyJogVel  = 60
+            m_joyJogVel  = ROBOT_SPEED_LINEAR
             
-        if msg.axes[7] == -1:
+        if msg.axes[AXIS_DIR_V] == -1:
             m_joyJogFlag = JOG_AXIS_TASK_X
-            m_joyJogVel  = -60
+            m_joyJogVel  = -ROBOT_SPEED_LINEAR
             
         r.jog(m_joyJogFlag, MOVE_REFERNECE_TOOL, m_joyJogVel)
 
     elif m_joyAnalogFlag and m_joyJogFlag == -1 and not m_joyButtonFlag:
-        if msg.axes[4] > 0:
+        if msg.axes[AXIS_RIGHT_V] > 0:
             m_joyJogFlag = JOG_AXIS_TASK_Z
-            m_joyJogVel = -60
-        if msg.axes[4] < 0:
+            m_joyJogVel = -ROBOT_SPEED_LINEAR
+        if msg.axes[AXIS_RIGHT_V] < 0:
             m_joyJogFlag = JOG_AXIS_TASK_Z
-            m_joyJogVel = 60
-        if msg.axes[1] > 0 and m_xyCompareFlag == 0:
+            m_joyJogVel = ROBOT_SPEED_LINEAR
+        if msg.axes[AXIS_LEFT_V] > 0 and m_xyCompareFlag == 0:
             m_joyJogFlag = JOG_AXIS_TASK_X
-            m_joyJogVel = 60
-        if msg.axes[1] < 0 and m_xyCompareFlag == 0:
+            m_joyJogVel = ROBOT_SPEED_LINEAR
+        if msg.axes[AXIS_LEFT_V] < 0 and m_xyCompareFlag == 0:
             m_joyJogFlag = JOG_AXIS_TASK_X
-            m_joyJogVel = -60
-        if msg.axes[0] > 0 and m_xyCompareFlag == 1:
+            m_joyJogVel = -ROBOT_SPEED_LINEAR
+        if msg.axes[AXIS_LEFT_H] > 0 and m_xyCompareFlag == 1:
             m_joyJogFlag = JOG_AXIS_TASK_Y
-            m_joyJogVel = -60
-        if msg.axes[0] < 0 and m_xyCompareFlag == 1:
+            m_joyJogVel = -ROBOT_SPEED_LINEAR
+        if msg.axes[AXIS_LEFT_H] < 0 and m_xyCompareFlag == 1:
             m_joyJogFlag = JOG_AXIS_TASK_Y
-            m_joyJogVel = 60
+            m_joyJogVel = ROBOT_SPEED_LINEAR
         r.jog(m_joyJogFlag, MOVE_REFERENCE_TOOL, m_joyJogVel)
 
     else:
